259ac445ae93666bf82e5a421a873607d09f4b95
Kenth Saya-ang <gargarkenth93@gmail.com>
2024-06-23 18:25:40 +0000

    initial commit

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  console.log(req.headers);
+
+  const ipaddress = req.headers['host'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    host, language, software
+  });
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
